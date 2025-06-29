b438266eb05e4245e485d312d57de18737e448ae
Phharoah <eng.ahmed7gazy@gmail.com>
2025-06-29 18:56:41 +0300

    first comm

--- expense_tracker_sys.dart
+++ expense_tracker_sys.dart
@@ -1,44 +1,4 @@
-import 'dart:io';
-// ignore_for_file: avoid_print
-
-void displayMenu() {
-  print('===== Expense Tracker ========');
-  print('1. Add Expense');
-  print('2. View All Expenses');
-  print('3. Exit');
-  print('==============================');
-}
-
-void run() {
-  bool running = true;
-
-  while (running) {
-    displayMenu();
-    stdout.write('Enter your choice (1-3): ');
-    var input = stdin.readLineSync();
-
-    try {
-      int choice = int.parse(input!);
-
-      switch (choice) {
-        case 1:
-          break;
-        case 2:
-          break;
-
-        case 3:
-          running = false;
-          print('Thank you for using Expense Tracker  System!');
-          break;
-
-        default:
-          print('Invalid choice. Please enter a number between 1 and 3');
-      }
-    } catch (e) {
-      print('Invalid input. Please enter a number.');
-    }
-  }
-}
+import 'menu.dart';
 
 void main() {
   run();
